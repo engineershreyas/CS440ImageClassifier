d9a46f7affe1f0b811396f8df1bf6cdd5dbd7348
shreyashirday@gmail.com <shreyashirday@gmail.com>
2016-12-18 16:43:09 -0500

    updated automation script to include every classifier

--- classification/automateCalls.py
+++ classification/automateCalls.py
@@ -1,14 +1,32 @@
 import os
 
-x = 4000
-while x <= 5000:
-    command = "python dataClassifier.py -c perceptron -t " + str(x)
+classifiers = ["naiveBayes","perceptron","mira"]
+
+for c in classifiers:
+    callBasedOnClassifier(c)
+
+def callBasedOnClassifier(classifier):
+    if classifier == "mira" or classifier == "naiveBayes":
+        classifier = classifier + " -a"
+    if classifier == "mira":
+        i = 1
+        while i <= 3:
+            classifier = classifier + " -i " + str(i)
+            doCalls(classifier)
+            i = i + 2
+    else:
+        doCalls(classifier)
+
+def doCalls(classifier):
+    x = 500
+    while x <= 5000:
+        command = "python dataClassifier.py -c " + classifier + " -f -t " + str(x)
+        os.system(command)
+        x = x + 500
+    y = 45
+    while y <= 405:
+        command = "python dataClassifier.py -c " + classifier + " -f -d faces -t " + str(y)
+        os.system(command)
+        y = y + 45
+    command = "python dataClassifier.py -c" + classifier + " -f -d faces -t 451"
     os.system(command)
-    x = x + 500
-y = 45
-while y <= 405:
-    command = "python dataClassifier.py -c perceptron -d faces -t " + str(y)
-    os.system(command)
-    y = y + 45
-command = "python dataClassifier.py -c perceptron -d faces -t 451"
-os.system(command)
